920c13db4902bafa3688fbc3211a8612e0ebccfc
zacscodingclub <zbaston@gmail.com>
2017-05-13 10:25:18 -0400

    add babel

--- index.js
+++ index.js
@@ -3,4 +3,38 @@ const initialState = {
   notes: {}
 }
 
+const onAddNote = () => {
+  const id = window.state.nextNoteId;
+  window.state.notes[id] = {
+    id,
+    content: ''
+  };
+  window.state.nextNoteId++;
+  renderApp();
+}
+
+const NoteApp = ({ notes }) => {
+  return (
+    <div>
+      <ul className="note-list">
+        {
+          Object.keys(notes).map(id => {
+            <li className="note-list-item" key={id}>
+              {id}
+            </li>
+          });
+        }
+      </ul>
+    </div>
+  );
+}
+
+const renderApp = () => {
+  ReactDOM.render(
+    <NoteApp notes={window.state.notes} />,
+    document.getElementById('root')
+  );
+};
+
 window.state = initialState;
+renderApp();
